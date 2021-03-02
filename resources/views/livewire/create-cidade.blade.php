f6d91ec61d810f2b3b5e5307de7f47016bfb7783
lucaspinheiro13 <lucasp2009@gmail.com>
2021-03-02 09:30:07 -0300

    Submit Function in blade

--- resources/views/livewire/create-cidade.blade.php
+++ resources/views/livewire/create-cidade.blade.php
@@ -9,7 +9,7 @@
     <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
   
     <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-      <form>
+      <form  wire:submit.prevent="store">
       <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
         <div class="">
               <div class="mb-4">
